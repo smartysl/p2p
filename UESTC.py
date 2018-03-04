67f7f9748b170aa158c94f30f87e74f40c36b4ae
smartysl <1632892359@qq.com>
2018-03-04 23:27:18 +0800

    上传

---
+++ UESTC.py
@@ -0,0 +1,26 @@
+import requests
+import re
+from bs4 import BeautifulSoup
+session=requests.session()
+headers={'User-Agent':'Mozilla/5.0(WindowsNT10.0;Win64;x64) AppleWebKit/537.36(KHTML,likeGecko)Chrome/63.0.3239.132Safari/537.36'
+}
+html=session.get("http://idas.uestc.edu.cn/authserver/login?service=http%3A%2F%2Fportal.uestc.edu.cn%2F").text
+ITlist=re.findall(r'LT-(.+?)-cas',html)
+IT="LT-"+ITlist[0]+"-cas"
+data={'username':'2017010901022','password':'990121','lt':IT,'dllt':'userNamePasswordLogin','execution':'e1s1','_eventId':'submit','rmShown':'1'}
+print(data)
+post=session.post("http://idas.uestc.edu.cn/authserver/login?service=http://portal.uestc.edu.cn/index.portal",data=data)
+session.get("http://eams.uestc.edu.cn/eams/home.action")
+session.get("http://eams.uestc.edu.cn/eams/courseTableForStd!courseTable.action")
+data1={ 'ignoreHead':1,
+    'setting.kind':'std',
+    'startWeek':1,
+    'semester.id':183,
+    'ids':148821}
+course=session.post("http://eams.uestc.edu.cn/eams/courseTableForStd!courseTable.action",data=data1).text
+soup=BeautifulSoup(course,"html.parser")
+courses=soup.find("tbody").children
+for i in courses:
+     print(i)
+session.get("http://eams.uestc.edu.cn/eams/logout.action?jsdEkingstar=1&redirect=http%3A%2F%2Fportal.uestc.edu.cn%2Flogout.portal")
+
---
+++ try.py
@@ -0,0 +1,71 @@
+import requests
+import re
+import time
+import os
+import sched
+from bs4 import BeautifulSoup
+class Click(object):
+    def __init__(self):
+        self.headers={'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36',
+                      'Referer':'http://bbs.uestc.edu.cn/member.php?mod=logging&action=login',
+                      'Content-Length':'155'}
+        self.session=requests.session()
+        self.schedule=sched.scheduler(time.time,time.sleep)
+    def time_exe(self,username,password,inc,cmd):
+        self.schedule.enter(inc,0,cmd,(username,password))
+        self.schedule.run()
+    def scarpy(self,username,password,referer='http://bbs.uestc.edu.cn/',loginfield='username',questionid='0',answer=""):
+        login_url='http://bbs.uestc.edu.cn/member.php?mod=logging&action=login'
+        r = self.session.get(login_url)
+        loginhtml = r.text
+        list = re.findall(r'formhash=(.+?)\'\,', loginhtml)
+        formhash=list[0]
+        data={'formhash':formhash,'referer':referer,'loginfield':loginfield,'username':username,'password':password,'questionid':questionid,'answer':answer}
+        posturl='http://bbs.uestc.edu.cn/member.php?mod=logging&action=login&loginsubmit=yes&loginhash=Lmqqj&inajax=1'
+        post=self.session.post(posturl,data=data,headers=self.headers,verify=False)
+        count=1
+        for i in range(1,1956):
+            pageurl='http://bbs.uestc.edu.cn/forum.php?mod=forumdisplay&fid=61&page='+str(i)
+            pagehtml = self.session.get(pageurl).text
+            soup=BeautifulSoup(pagehtml,"html.parser")
+            titles=soup.findAll('a',attrs={'class':'s xst'})
+            with open('ysl.txt','a',encoding='utf-8') as f:
+                for title in titles:
+                    print("抓取%d条"%count)
+                    count=count+1
+                    f.write("\n")
+                    f.write(title.string)
+    def autorespones(self,username,password,referer='http://bbs.uestc.edu.cn/',loginfield='username',questionid='0',answer=""):
+        login_url = 'http://bbs.uestc.edu.cn/member.php?mod=logging&action=login'
+        r = self.session.get(login_url)
+        loginhtml = r.text
+        list = re.findall(r'formhash=(.+?)\'\,', loginhtml)
+        formhash = list[0]
+        data = {'formhash': formhash, 'referer': referer, 'loginfield': loginfield, 'username': username,
+                'password': password, 'questionid': questionid, 'answer': answer}
+        posturl = 'http://bbs.uestc.edu.cn/member.php?mod=logging&action=login&loginsubmit=yes&loginhash=Lmqqj&inajax=1'
+        post = self.session.post(posturl, data=data, headers=self.headers, verify=False)
+        for page in range(1,3):
+            pageurl = 'http://bbs.uestc.edu.cn/forum.php?mod=forumdisplay&fid=61&page='+str(page)
+            pagehtml=self.session.get(pageurl).text
+            thread=re.findall(r'id="normalthread_(.+?)\"',pagehtml)
+            for i in thread:
+               print(i)
+               titleurl="http://bbs.uestc.edu.cn/forum.php?mod=viewthread&tid="+i+"&extra=page%3D1"
+               titlehtml=self.session.get(titleurl).text
+               form=re.findall(r'formhash=(.+?)\"',titlehtml)
+               timing=re.findall(r'id="posttime" value="(.+?)\"',titlehtml)
+               aimurl='http://bbs.uestc.edu.cn/forum.php?mod=post&action=reply&fid=61&tid='+i+'&extra=page%3D1&replysubmit=yes&infloat=yes&handlekey=fastpost&inajax=1'
+               autoresponsedata={'message':'我蒋某人祝大家新年快乐','posttime':timing[0],'formhash':form[0],'usesig':'1','subject':''}
+               print(autoresponsedata)
+               response=self.session.post(aimurl,data=autoresponsedata,headers=self.headers,verify=False)
+               time.sleep(0.1)
+if __name__=='__main__':
+    username="诡异的守护者"
+    password="YZNyzn194612"
+    c=Click()
+    c.scarpy(username,password)
+
+
+
+
